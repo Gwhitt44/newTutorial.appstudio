8a0f212561d74f11b8113569a270069006e88f12
Grant Whittelsey <gmw92396@creighton.edu>
2021-09-15 14:15:03 -0500

    Create Form1.js

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,5 @@
+// put below the first line of code already there
+let myFirstName = prompt("Enter your first name: ")
+console.log(`Your first name is ${myFirstName}.`)
+let myLastName = prompt("Enter your last name: ")
+console.log(`Your first name is ${myLastName}.`)
